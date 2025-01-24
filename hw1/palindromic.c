46dee52047741153642d4e21e718c2917cbecb92
Alexander Rocabado Hellgren <alrh@kth.se>
2025-01-24 10:47:39 +0100

    removed redundant condition variable, now uses value in join() instead

--- hw1/palindromic.c
+++ hw1/palindromic.c
@@ -10,12 +10,14 @@
 #define READ_MAX 16384
 #define AMOUNT_OF_WORDS 101921 
 #define BUFFER_INDEX_SIZE 100
+#define MAX_ACTIVE_THREADS 1
 
 FILE *file_ptr;
 FILE *write_file;
 int NTHREADS;
 int amount_of_palindromics;
 char input_buffer[BUFFER_INDEX_SIZE*AMOUNT_OF_WORDS];
+int active_threads = 0;
 
 struct thread_indices {
 	int start;
@@ -30,7 +32,7 @@ struct saved_indices_s {
 };
 
 struct timespec t_start, t_stop;
-pthread_mutex_t mutexwrite;
+pthread_mutex_t mutexwrite = PTHREAD_MUTEX_INITIALIZER;
 
 void *find_palindromic(void *);
 void read_file(char *);
@@ -55,7 +57,10 @@ int main(int argc, char *argv[]){
 		exit(3);
 	}
 	printf("amount of threads: %d\n", NTHREADS);
+
 	read_file("british-english");
+
+	int thread_sum = 0;
 	struct thread_indices thread_indices_array[NTHREADS];
 	struct thread_indices *indices;
 	int thread_loop_limit = AMOUNT_OF_WORDS / NTHREADS;
@@ -63,9 +68,9 @@ int main(int argc, char *argv[]){
 	int found_words_indices[AMOUNT_OF_WORDS];
 	saved_indices.index = 0;
 	pthread_t thread_id[NTHREADS];
-	pthread_mutex_init(&mutexwrite, NULL);
 	struct timeval start;
 	struct timeval stop;
+
 	clock_gettime(CLOCK_MONOTONIC, &t_start);	
 	for(int i = 0; i < NTHREADS; i++){
 		if(i == 0){
@@ -81,22 +86,29 @@ int main(int argc, char *argv[]){
 		thread_indices_array[i].found= 0;
 		pthread_create( &thread_id[i], NULL, find_palindromic, (void *) &thread_indices_array[i]);
 	}
+
 	for(int j = 0; j < NTHREADS; j++){
-		pthread_join(thread_id[j], NULL);
+		void *sum = NULL;
+		pthread_join(thread_id[j], &sum);
+		thread_sum += *(int *)sum;
 	}
+	printf("sum from threads: %d\n", thread_sum);
+
+	printf("verify that threads return value and global variable, if == 0 it was successful. val: %d\n", amount_of_palindromics - thread_sum);
+
 	for(int j = 0; j < NTHREADS; j++){
 		fprintf(write_file, "A total of %d palindromics found by thread %d\n", thread_indices_array[j].found, thread_indices_array[j].t_id);
 	}
 	for(int i = 0; i < saved_indices.index; i++){
 		char word[20]; 
 		strcpy(word, &input_buffer[saved_indices.found_indices[i] * BUFFER_INDEX_SIZE]);
-		printf("index in the struct: %d\n", saved_indices.index);
+		//printf("index in the struct: %d\n", saved_indices.index);
 		fprintf(write_file, "%s\n", word);
 	}
 	clock_gettime(CLOCK_MONOTONIC, &t_stop);
 	long total_time = nano_seconds(&t_start, &t_stop);
 	printf("total time: %0.2f seconds for %d threads\n", (double)total_time/NANO, NTHREADS);
-	fprintf(write_file, "total time: %0.2f seconds for %d threads to find %d palindromics\n", (double)total_time / NANO, NTHREADS, amount_of_palindromics);
+	fprintf(write_file, "total time: %0.2f seconds for %d threads to find %d palindromics\n", (double)total_time / NANO, NTHREADS, thread_sum);
 	fclose(write_file);
 	pthread_mutex_destroy(&mutexwrite);
 	return 0;
@@ -115,10 +127,11 @@ int lookup(char *word){
 	for(int i = 0; i < AMOUNT_OF_WORDS; i++){
 		char *temp = &input_buffer[i * BUFFER_INDEX_SIZE];
 		if(strcasecmp(word, temp) == 0){
-			return 0;	
+			//printf("found: %s, \n", word);
+			return 1;	
 		}
 	}
-	return 1;
+	return 0;
 }
 
 void print_read_file(){
@@ -144,6 +157,7 @@ void read_file(char *filename){
 }
 
 void *find_palindromic(void *threadarg){
+	int *thread_found = malloc(sizeof(int));
 	struct thread_indices *my_limits;
 	my_limits = (struct thread_indices *) threadarg;
 	int start = my_limits->start;
@@ -162,14 +176,18 @@ void *find_palindromic(void *threadarg){
 			r--;
 		}
 		int res = lookup(word);
-		if(res == 0){
+		if(res == 1){
 			pthread_mutex_lock(&mutexwrite);
+			active_threads++;
 			amount_of_palindromics++;
 			saved_indices.found_indices[saved_indices.index] = start;
 			saved_indices.index++;
+			active_threads--;
 			pthread_mutex_unlock(&mutexwrite);
 			my_limits->found++;
+			(*thread_found)++;
 		}
 	}
 	printf("A total of %d palindromics found by %d\n", my_limits->found, my_limits->t_id);
+	pthread_exit((void *) thread_found);
 }
